e00a35f651b11047b2091aa95331064d941c259b
VuVietDuc <vuvietduc1998@gmail.com>
2020-07-22 22:26:27 +0700

    create comment

--- src/main/java/controller/CommentController.java
+++ src/main/java/controller/CommentController.java
@@ -50,66 +50,7 @@ public class CommentController {
 		}
 	}
 
-	@PostMapping(value = "comment/edit/{id}")
-	@ResponseBody
-	public Integer update(ModelMap mode, @PathVariable("id") int id, @RequestParam("image") MultipartFile image,
-			@RequestParam("content") String content, @RequestParam("c_id") int c_id,
-			@RequestParam("c_user") int c_user) {
-		if (user.userID() == c_user) {
-			String url = upload.uploadImage(mode, image);
-			String image_path = url.equals("") ? commentDaoimpl.findByID(c_id).getImage_url() : url;
-			commentDaoimpl.Update(new Comments(c_id, postDaoimpl.findByID(id), user.getCurrentUsers(), content,
-					image_path, "NULL", timestamp.toString()));
-			return size(c_id, "Comment");
-		} else {
-			return size(c_id, "Comment");
-		}
-
-	}
-
-	@PostMapping(value = "comment/delete/{id}")
-	@ResponseBody
-	public Integer Delete(ModelMap mode, @PathVariable("id") int id, @RequestParam("c_user") int c_user) {
-		int size = postDaoimpl.findByID(commentDaoimpl.findByID(id).post.getId()).comments.size();
-		if (user.userID() == c_user) {
-			commentDaoimpl.Delete(id);
-			return (size - 1);
-		} else {
-			return (size - 1);
-		}
-
-	}
-
-	@GetMapping(value = "admin/comment/list")
-	public String list(ModelMap model) {
-
-		if (user.isAdminOrMod()) {
-			model.addAttribute("comments", commentDaoimpl.list());
-			return "admin/comment/list";
-		} else {
-			return "auth/401";
-		}
-	}
-
-	@GetMapping(value = "admin/comment/delete/{id}")
-	public String DeleteAdmin(ModelMap mode, @PathVariable("id") int id) {
-
-		if (user.isAdminOrMod()) {
-			if (commentDaoimpl.Delete(id)) {
-				mode.addAttribute("msg", "Thao tác thành công.");
-				mode.addAttribute("class_name", "msg_success");
-			} else {
-				mode.addAttribute("msg", "Thao tác thất bại.");
-				mode.addAttribute("class_name", "msg_error");
-			}
-			mode.addAttribute("comments", commentDaoimpl.list());
-			return "admin/comment/list";
-		} else {
-			return "auth/401";
-		}
-
-	}
-
+	
 	public int size(int p_id, String type) {
 		int rls = type.equals("Comment")
 				? postDaoimpl.findByID(commentDaoimpl.findByID(p_id).post.getId()).comments.size()
