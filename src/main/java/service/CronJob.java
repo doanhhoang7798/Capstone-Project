58561d25729756ed9d8623691aa29f51ff65a560
Le Dieu Ly <ledieuly0504@gmail.com>
2020-08-06 15:51:46 +0700

    fix dateunlock user

---
+++ src/main/java/service/CronJob.java
@@ -0,0 +1,46 @@
+package service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import dao.ReportDao;
+import dao.UserDao;
+import model.Users;
+
+public class CronJob {
+
+	@Autowired
+	UserDao userDaoimpl;
+
+	@Autowired
+	ReportDao reportDaoimpl;
+
+	@Scheduled(cron = "0 0 0 * * ?")
+
+	public void autoUnlockUser() throws ParseException {
+
+		for (Users user : userDaoimpl.filterByStatus(5)) {
+			Date today = new Date();
+
+			SimpleDateFormat fomart = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+			Date block_date = fomart.parse(user.getBlock_date());
+			Date unlock_date = new Date(block_date.getTime() + (1000 * 60 * 60 * 24));
+
+			if ((fomart.format(today)).equals(fomart.format(unlock_date))) {
+				userDaoimpl.setStatus(user.getId(), 1, null);
+				reportDaoimpl.deleteMany(user.getId());
+
+			} else {
+				System.out.println("nothing to do ...");
+			}
+		}
+
+	}
+
+
+}
